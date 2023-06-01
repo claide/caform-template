2b8d6e407514cd91b63b18ec85394c3c0774b6da
John Villacorta <rosemalejohn@gmail.com>
2023-06-01 16:47:44 +0800

    Resolve bug on file uploader

--- components/AppFileUploader.vue
+++ components/AppFileUploader.vue
@@ -6,44 +6,25 @@
     <div class="mt-1">
       <div class="relative overflow-hidden inline-block hover:cursor-pointer">
         <button
-          class="bg-gray-50 border border-gray-300 focus:ring-primary focus:border-primary block w-full text-base rounded py-1.5 px-6"
-        >
+          class="bg-gray-50 border border-gray-300 focus:ring-primary focus:border-primary block w-full text-base rounded py-1.5 px-6">
           Select files
         </button>
-        <input
-          multiple
-          type="file"
-          name="document"
-          accept=".doc,.docx,.pdf,.png,.jpg,.jpeg"
-          class="absolute left-0 top-0 opacity-0 hover:cursor-pointer"
-          @change="handleFileUpload"
-        />
+        <input multiple type="file" name="document" accept=".doc,.docx,.pdf,.png,.jpg,.jpeg"
+          class="absolute left-0 top-0 opacity-0 hover:cursor-pointer" @change="handleFileUpload" />
       </div>
     </div>
 
     <div class="mt-3">
-      <span
-        v-for="(file, index) in files"
-        :key="file.name"
-        class="inline-flex items-center px-2 py-1 mr-2 mb-2 text-sm font-medium text-indigo-800 bg-indigo-100 rounded dark:bg-indigo-900 dark:text-indigo-300"
-      >
+      <span v-for="(file, index) in files" :key="file.name"
+        class="inline-flex items-center px-2 py-1 mr-2 mb-2 text-sm font-medium text-indigo-800 bg-indigo-100 rounded dark:bg-indigo-900 dark:text-indigo-300">
         {{ file.name || file.file_name }}
-        <button
-          @click="removeFile(index)"
-          class="nline-flex items-center p-0.5 ml-2 text-sm text-indigo-400 bg-transparent rounded-sm hover:bg-indigo-200 hover:text-indigo-900 dark:hover:bg-indigo-800 dark:hover:text-indigo-300"
-        >
-          <svg
-            class="w-3.5 h-3.5"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
+        <button @click="removeFile(index)"
+          class="nline-flex items-center p-0.5 ml-2 text-sm text-indigo-400 bg-transparent rounded-sm hover:bg-indigo-200 hover:text-indigo-900 dark:hover:bg-indigo-800 dark:hover:text-indigo-300">
+          <svg class="w-3.5 h-3.5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd"
               d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            ></path>
+              clip-rule="evenodd"></path>
           </svg>
         </button>
       </span>
@@ -69,7 +50,7 @@ const handleFileUpload = (event) => {
 };
 
 const removeFile = (index) => {
-  files.value = files.value.filter((file, i) => i !== index);
+  // files.value = files.value.filter((file, i) => i !== index);
   emits("remove-file", index);
 };
 
